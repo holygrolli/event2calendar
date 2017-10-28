5922f9b4dbf94725bb6adb1670b1183b7fd7c115
Andreas Groll <andreas.groll@gmail.com>
2017-10-28 13:41:34 +0200

    getting cfn package to work with helper script

--- cfn/prepare_template.sh
+++ cfn/prepare_template.sh
@@ -4,4 +4,4 @@ set -o errexit
 
 account_id="$(aws sts get-caller-identity --query Account --output text | xargs echo -n)"
 echo using account ${account_id} in ${AWS_REGION}
-sed -e "s/REGION/${AWS_REGION}/g" -e "s/ACCOUNT_ID/${account_id}/g" api-template.yaml > api.yaml
+sed -e "s/REGION/${AWS_REGION}/g" -e "s/ACCOUNT_ID/${account_id}/g" api-template.yml > api.yml
