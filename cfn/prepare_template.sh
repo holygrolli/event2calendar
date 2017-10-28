881c03882903ef5ce35a1e252037c8ef173565e6
Andreas Groll <andreas.groll@gmail.com>
2017-10-28 13:57:50 +0200

    getting cfn package to work with helper script

--- cfn/prepare_template.sh
+++ cfn/prepare_template.sh
@@ -5,4 +5,3 @@ set -o errexit
 account_id="$(aws sts get-caller-identity --query Account --output text | xargs echo -n)"
 echo using account ${account_id} in ${AWS_REGION}
 sed -e "s/REGION/${AWS_REGION}/g" -e "s/ACCOUNT_ID/${account_id}/g" api-template.yml > api.yml
-ls -al ../lambda.backend/target/
