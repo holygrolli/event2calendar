c52520de8edc5eaacab04fa47956329e5850ce48
Andreas Groll <andreas.groll@gmail.com>
2017-11-03 01:10:34 +0100

    changed url encoding to url safe base64

--- lambda-modules/lambda.backend/src/main/java/de/networkchallenge/e2c/lambda/backend/parser/impl/util/B64UrlDecoder.java
+++ lambda-modules/lambda.backend/src/main/java/de/networkchallenge/e2c/lambda/backend/parser/impl/util/B64UrlDecoder.java
@@ -1,14 +1,13 @@
 package de.networkchallenge.e2c.lambda.backend.parser.impl.util;
 
-import java.net.URLDecoder;
 import java.util.Base64;
 import java.util.Optional;
 
 public class B64UrlDecoder {
-    public static Optional<String> parse(String base64urlEncodedString)
+    public static Optional<String> parse(String base64urlsafeString)
     {
         try {
-            return Optional.of(new String(Base64.getDecoder().decode(URLDecoder.decode(base64urlEncodedString, "UTF-8"))));
+            return Optional.of(new String(Base64.getUrlDecoder().decode(base64urlsafeString)));
         } catch (Exception e) {
             return Optional.empty();
         }
--- lambda-modules/lambda.backend/src/test/java/de/networkchallenge/e2c/lambda/backend/parser/impl/util/B64UrlDecoderTest.java
+++ lambda-modules/lambda.backend/src/test/java/de/networkchallenge/e2c/lambda/backend/parser/impl/util/B64UrlDecoderTest.java
@@ -3,14 +3,16 @@ package de.networkchallenge.e2c.lambda.backend.parser.impl.util;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class B64UrlDecoderTest {
     @Test
     public void parse() throws Exception {
-        assertEquals("https://films2017.dok-leipzig.de/de/film/?ID=16412",B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZGUvZmlsbS8%2FSUQ9MTY0MTI%3D").get());
-        assertEquals("https://films2017.dok-leipzig.de/de/film/?ID=16412",B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZGUvZmlsbS8/SUQ9MTY0MTI%3D").get());
-        assertEquals("https://films2017.dok-leipzig.de/de/film/?ID=16412",B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZGUvZmlsbS8/SUQ9MTY0MTI=").get());
-        assertEquals("https://films2017.dok-leipzig.de/en/film/?ID=16502&title=Airport",B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZW4vZmlsbS8%2FSUQ9MTY1MDImdGl0bGU9QWlycG9ydA%3D%3D").get());
+        assertFalse(B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZGUvZmlsbS8%2FSUQ9MTY0MTI%3D").isPresent());
+        assertFalse(B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZGUvZmlsbS8/SUQ9MTY0MTI%3D").isPresent());
+        assertEquals("https://films2017.dok-leipzig.de/de/film/?ID=16412",B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZGUvZmlsbS8_SUQ9MTY0MTI=").get());
+        assertEquals("https://films2017.dok-leipzig.de/en/film/?ID=16502&title=Airport",B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZW4vZmlsbS8_SUQ9MTY1MDImdGl0bGU9QWlycG9ydA==").get());
+        assertEquals("https://films2017.dok-leipzig.de/de/film/?ID=18371&title=Betrug",B64UrlDecoder.parse("aHR0cHM6Ly9maWxtczIwMTcuZG9rLWxlaXB6aWcuZGUvZGUvZmlsbS8_SUQ9MTgzNzEmdGl0bGU9QmV0cnVn").get());
     }
 
 }
