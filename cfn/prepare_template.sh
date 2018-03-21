b31f332d95af71718bfce2df697b3cc80289cb30
Andreas Groll <andreas.groll@gmail.com>
2018-03-21 22:24:23 +0100

    fixed region

--- cfn/prepare_template.sh
+++ cfn/prepare_template.sh
@@ -4,4 +4,4 @@ set -o errexit
 
 account_id="$(aws sts get-caller-identity --query Account --output text | xargs echo -n)"
 echo using account ${account_id} in ${AWS_DEFAULT_REGION}
-sed -e "s/REGION/${AWS_REGION}/g" -e "s/ACCOUNT_ID/${account_id}/g" api-template.yml > api.yml
+sed -e "s/REGION/${AWS_DEFAULT_REGION}/g" -e "s/ACCOUNT_ID/${account_id}/g" api-template.yml > api.yml
